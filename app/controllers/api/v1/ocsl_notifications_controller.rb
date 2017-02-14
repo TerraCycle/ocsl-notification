e2b7df129153e11bdec42ec004fea7d529fce5a1
Kris Birov <kris.birov@gmail.com>
2017-02-14 09:52:11 +0100

    response temp fix

--- app/controllers/api/v1/ocsl_notifications_controller.rb
+++ app/controllers/api/v1/ocsl_notifications_controller.rb
@@ -1,10 +1,11 @@
 module Api
   module V1
     class OcslNotificationsController < ApplicationController
+      skip_before_action :verify_authenticity_token
 
       # POST
       def create
-        render json: { status: 200, data: params[:data][:attributes], message: nil }
+        render json: { status: 200, data: params.symbolize_keys[:data], message: nil }
       end
     end
   end
