70d6099d499a30c5f68b32c79202bd03f6cad4b0
Kris Birov <kris.birov@gmail.com>
2017-02-24 13:36:42 +0100

    basic auth route

--- app/controllers/api/v1/ocsl_notifications_controller.rb
+++ app/controllers/api/v1/ocsl_notifications_controller.rb
@@ -2,18 +2,27 @@ module Api
   module V1
     class OcslNotificationsController < ApplicationController
       skip_before_action :verify_authenticity_token
+      http_basic_authenticate_with name: 'test', password: 'auth', only: [:ocsl_notification_basic_auth]
+      before_action :create_request
 
-      # POST
       def create
+        render json: { status: 200, data: @data[:attributes], message: nil }
+      end
+
+      def ocsl_notification_basic_auth
+        render json: { status: 200, data: @data[:attributes], message: 'BASIC AUTH' }
+      end
+
+      private
+
+      def create_request
         orig_params = params.symbolize_keys[:data]
         if orig_params['token'].nil?
-          data = orig_params.merge(token: 'test').symbolize_keys
+          @data = orig_params.merge(token: 'test').symbolize_keys
         else
-          data = JSON.parse(params.symbolize_keys[:data], symbolize_names: true)
+          @data = JSON.parse(params.symbolize_keys[:data], symbolize_names: true)
         end
-        Request.create!(token: data[:token], body: data[:attributes])
-
-        render json: { status: 200, data: data[:attributes], message: nil }
+        Request.create!(token: @data[:token], body: @data[:attributes])
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :ocsl_notifications, only: [:create]
+      post '/ocsl_notification_basic_auth' => 'ocsl_notifications#ocsl_notification_basic_auth'
     end
   end
 end
